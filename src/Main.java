6a31b4166012b09f6f0e1a4752c2df7a899c0ab6
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-17 11:02:53 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    public static <T extends Comparable<T>> T min(T a, T b) {
+        if ( a.compareTo(b) > 0 ) return b;
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Mitarbeiter m1 = new Mitarbeiter(1, "Müller", "Einkauf");
+        Mitarbeiter m2 = new Mitarbeiter(2, "Schmitt", "Verkauf");
+        Mitarbeiter m3 = new Mitarbeiter(3, "Fritz", "Einkauf");
+        //Mitarbeiter m4 = new Mitarbeiter(4, "Fritz", "Einkauf");
+
+        //System.out.println( min(m1, m2) );
+
+        ArrayList<Mitarbeiter> mitarbeiterList = new ArrayList<>();
+        mitarbeiterList.add(m1);
+        mitarbeiterList.add(m2);
+        mitarbeiterList.add(m3);
+        System.out.println(mitarbeiterList);
+
+        mitarbeiterList.sort( new Mitarbeiter.AbteilungsComparator() );
+        System.out.println(mitarbeiterList);
+
+        mitarbeiterList.sort(new Comparator<Mitarbeiter>() {
+            @Override
+            public int compare(Mitarbeiter a, Mitarbeiter b) {
+                return b.getMnr() - a.getMnr();
+            }
+        });
+        System.out.println(mitarbeiterList);
+
+        mitarbeiterList.sort( (a, b) -> b.getName().compareTo(a.getName()) );
+        System.out.println(mitarbeiterList);
+
+        mitarbeiterList.sort(Comparator.comparingInt(Mitarbeiter::getMnr));
+        System.out.println(mitarbeiterList);
+
+        mitarbeiterList.sort( Comparator.reverseOrder() );
+        System.out.println(mitarbeiterList);
+
+
+//        // Dynamisches Array mit Strings
+//        ArrayList<String> stringList = new ArrayList<>();
+//
+//        stringList.add( "Hallo" );
+//        stringList.add( "Wie" );
+//        stringList.add( "gehts" );
+//        stringList.add( "denn?" );
+//
+//        System.out.println(stringList);
+    }
+
+}
---
+++ src/Mitarbeiter.java
@@ -0,0 +1,86 @@
+import java.util.Comparator;
+
+public class Mitarbeiter implements Comparable<Mitarbeiter> {
+    private int mnr;
+    private String name;
+    private String abteilung;
+
+    public static class AbteilungsComparator implements Comparator<Mitarbeiter> {
+
+        // Wie funktioniert compare(a,b) ?
+        // Rückgabe > 0 wenn  a > b
+        //          = 0 wenn  a = b
+        //          < 0 wenn  a < b
+        @Override
+        public int compare(Mitarbeiter a, Mitarbeiter b) {
+            if (a.abteilung.equals(b.abteilung)) {
+                // 2. Ordnung
+                if (a.name.equals(b.name)) {
+                    // 3. Ordnung = natürliche Ordnnug
+                    return a.compareTo(b);
+                }
+                return a.name.compareTo(b.name);
+            }
+            return a.abteilung.compareTo(b.abteilung);
+        }
+    }
+
+
+    public Mitarbeiter(int mnr, String name, String abteilung) {
+        this.mnr = mnr;
+        this.name = name;
+        this.abteilung = abteilung;
+    }
+
+    public int getMnr() {
+        return mnr;
+    }
+
+    public void setMnr(int mnr) {
+        this.mnr = mnr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAbteilung() {
+        return abteilung;
+    }
+
+    public void setAbteilung(String abteilung) {
+        this.abteilung = abteilung;
+    }
+
+    // Wie funktioniert compareTo ?
+    // Rückgabe > 0 wenn this > that
+    //          = 0 wenn this = that
+    //          < 0 wenn this < that
+    @Override
+    public int compareTo(Mitarbeiter that) {
+        // wenn "natürliche Ordnung" über Mitarbeiternummer definiert wird
+        return this.mnr - that.mnr;
+//        if (this.mnr > that.mnr)  return 1;
+//        if (this.mnr == that.mnr) return 0;
+//        if (this.mnr < that.mnr)  return -1;
+        // wenn "nO" über Mitarbeitername definiert wird
+        //return this.name.compareTo(that.name);
+        // wenn "nO" über Mitarbeitername eine totale Ordnung sein soll
+//        if (this.name.equals(that.name)) {
+//            // Sortierung nach 2. Ordnung
+//            return this.mnr - that.mnr;
+//        }
+//        else
+//            // Sortierung nach 1. Ordnung
+//            return this.name.compareTo(that.name);
+    }
+
+    @Override
+    public String toString() {
+        return mnr + ":" + name + "(" + abteilung + ")";
+    }
+}
